ea507869d69fd8f9f821606ddd0f5c38ced0ca6b
humblefirm <38067691+humblefirm@users.noreply.github.com>
2021-10-12 17:25:48 +0900

    feat: sign module complete

---
+++ src/utils/sign.js
@@ -0,0 +1,16 @@
+var crypto = require("crypto");
+
+// THIS COMMENT IS AN EXAMPLE!
+//
+// sign(
+//   "0xc1912fEE45d61C87Cc5EA59DaE31190FFFFf232d|xoq|10|xoq|1634006614103",
+//   "mock"
+// );
+
+function sign(plain, salt) {
+  const hash = crypto.createHmac("sha256", salt).update(plain).digest("string");
+  const signature = hash.toString("base64");
+  console.log(signature);
+}
+
+module.exports = sign;
